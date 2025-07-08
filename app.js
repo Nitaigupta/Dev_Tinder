a7b33b8c470a1885f6fc6dc7dd887144b45ecada
NitaiGupta <nitaigupta2003@gmail.com>
2025-07-08 22:58:27 +0530

    Connected to the database

--- app.js
+++ app.js
@@ -1,12 +1,35 @@
 const express = require('express');
+const connectDB = require('./config/database');
 require("./config/database")
 const app = express();
+const User = require("./models/User")
 
+app.post('/signup', async (req,res)=>{
+  const userObj = {
+    firstName:"Shweta",
+    lastName : "Gupta",
+    emailId  : "sg@gmail.com",
+    password : "4321"
 
-app.use("/",(req,res)=>{
-  res.send("Hello");
+  }
+  //creating a new instance of user model
+  const user = new User(userObj);
+  try{
+    await user.save();
+  res.send("user Added");
+  }
+  catch(err){
+    res.status(400).send("Error saving the user:"+ err.message);
+  }
 
-})
-app.listen(3000, () => {
+});
+
+connectDB().then(()=>{
+    console.log("Databse connected Succesfully");
+    app.listen(3000, () => {
   console.log('Server is running on http://localhost:3000');
 });
+}).catch((err)=>{
+    console.log("Error While Connecting To DataBase");
+});
+

ba09cce78f9d7937b82a05d13f87ae9a27e4cba5
NitaiGupta <nitaigupta2003@gmail.com>
2025-07-26 20:46:21 +0530

    Created api using express router

--- models/User.js
+++ models/User.js
@@ -1,10 +1,14 @@
 const mongoose = require('mongoose');
-const validator = require('validator')
+const validator = require('validator');
+const jwt = require('jsonwebtoken');
+const bcrypt = require("bcrypt");
 
 const userSchema = new mongoose.Schema({
     firstName :{
         type:String,
         required:true,
+        minLength:4,
+        maxLength:50,
     },
     lastName :{
         type:String
@@ -63,6 +67,16 @@ const userSchema = new mongoose.Schema({
 },{
     timestamps:true,
 });
-
+userSchema.methods.getJWT = async function(){
+    const user = this;
+    const token= await jwt.sign({_id:user._id},"DEV@Tinder$790",{expiresIn:"7d"});
+    return token;
+};
+userSchema.methods.validatePassword = async function(passwordInputByUser){
+    const user = this;
+    const passwordHash = user.password;
+    const isPasswordValid = bcrypt.compare(passwordInputByUser,passwordHash);
+    return isPasswordValid;
+}
 const User =mongoose.model("User",userSchema);
 module.exports = User;
---
+++ routes/profile.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const { userAuth } = require('../middleware/auth');
+const profileRouter = express.Router();
+const User = require("../models/User");
+const{validateEditProfileData} = require('../utils/validation')
+
+profileRouter.get('/profile/view',userAuth,async(req,res)=>{
+    try{
+        const user = req.user;
+        res.send(user);
+    }
+    catch(err){
+        res.status(400).send("Error"+ err.message);
+    }
+});
+profileRouter.patch('/profile/edit', userAuth , async(req,res)=>{
+    try{
+        if(!validateEditProfileData(req)){
+            throw new Error("Invalid Edit Request");
+        }
+        const loggedInUser= req.user;
+       Object.keys(req.body).forEach((key)=>(loggedInUser[key] = req.body[key]));
+       await loggedInUser.save();
+       res.json({
+        message:`${loggedInUser.firstName} your profile was Updated Successfully`,
+        data:loggedInUser
+       })
+
+        
+    }
+    catch(err){
+        res.status(400).send("Error :"+err.message);
+    }
+})
+
+module.exports = profileRouter;
